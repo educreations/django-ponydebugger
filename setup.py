e4d031f71495356e8e2b52c5485fc824e730bf1c
Chris Streeter <chris@chrisstreeter.com>
2013-01-25 11:52:49 -0800

    Close files and read in full license file.

--- setup.py
+++ setup.py
@@ -3,11 +3,18 @@ from setuptools import setup
 
 import django_ponydebugger
 
+
+with open(join(dirname(__file__), 'README.rst')) as f:
+    readme = f.read()
+
+with open(join(dirname(__file__), 'LICENSE')) as f:
+    license = f.read()
+
 setup(
     name='django-ponydebugger',
     version=django_ponydebugger.__version__,
     description='PonyDebugger support for Django',
-    long_description=open(join(dirname(__file__), 'README.rst')).read(),
+    long_description=readme,
     packages=[
         'django_ponydebugger',
         'django_ponydebugger.domains',
@@ -21,7 +28,7 @@ setup(
     author='Matthew Eastman',
     author_email='matt@educreations.com',
     url='https://github.com/educreations/django-ponydebugger',
-    license='MIT',
+    license=license,
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
